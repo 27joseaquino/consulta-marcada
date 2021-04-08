540fe37070fd805fb09cd8cde9bda2c7583c9547
Dhiana <dhianaslv@outlook.com>
2021-04-08 19:06:18 -0300

    Add: alerta para sair do app

---
+++ lib/ui/components/custom_alert.dart
@@ -0,0 +1,89 @@
+import 'package:consulta_marcada/ui/components/buttons/custom_button.dart';
+import 'package:consulta_marcada/ui/styles/custom_text.dart';
+import 'package:flutter/material.dart';
+
+class CustomAlert {
+  Future<void> alert({
+    BuildContext context,
+    String title,
+    String message,
+    Function function,
+  }) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: CustomText(
+            text: title,
+            fontSize: 18,
+            textAlign: TextAlign.justify,
+            fontWeight: FontWeight.bold,
+            maxlines: 2,
+          ),
+          content: CustomText(
+            text: message,
+            fontSize: 18,
+            maxlines: 5,
+            textAlign: TextAlign.justify,
+          ),
+          actions: [
+            CustomButton(
+              title: "Entendi",
+              onPressed: function ?? () => Navigator.pop(context),
+              height: 50,
+              width: double.infinity,
+              fontSize: 18,
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Future<void> dialog({
+    BuildContext context,
+    String title,
+    String message,
+    String firstButtonTitle,
+    String secondButtonTitle,
+    Function function,
+  }) {
+    return showDialog<void>(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: CustomText(
+            text: title,
+            fontSize: 18,
+            maxlines: 2,
+            textAlign: TextAlign.justify,
+            fontWeight: FontWeight.bold,
+          ),
+          content: CustomText(
+            text: message,
+            fontSize: 18,
+            maxlines: 5,
+            textAlign: TextAlign.start,
+          ),
+          actions: <Widget>[
+            CustomButton(
+              onPressed: () => Navigator.pop(context),
+              title: firstButtonTitle,
+              fontSize: 18,
+              height: 50,
+              width: 100,
+              hasBorder: true,
+            ),
+            CustomButton(
+              onPressed: function,
+              title: secondButtonTitle,
+              fontSize: 18,
+              height: 50,
+              width: 100,
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
